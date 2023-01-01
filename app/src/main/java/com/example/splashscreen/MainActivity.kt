401880324a6bbb13bf16d2392806b4dc202680af
destanuarisufiaa <destanuarisufia25@gmail.com>
2023-01-01 15:28:01 +0700

    progress login sampai fragment

--- app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.splashscreen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.splashscreen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.riwayat -> replaceFragment(riwayat())
+                R.id.profile -> replaceFragment(profile())
+
+                else ->{
+
+                }
+            }
+
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
--- app/src/main/java/com/example/splashscreen/RegisterActivity2.kt
+++ app/src/main/java/com/example/splashscreen/RegisterActivity2.kt
@@ -1,24 +1,37 @@
 package com.example.splashscreen
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import android.widget.Toast
 import com.example.splashscreen.databinding.ActivityRegister2Binding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity2 : AppCompatActivity() {
 
     lateinit var binding :ActivityRegister2Binding
     lateinit var auth : FirebaseAuth
+    lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityRegister2Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val gender = findViewById<TextView>(R.id.txt_gender_register)
+        val gender1 = findViewById<RadioGroup>(R.id.rg_gender1_register)
+
         auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         binding.tvToLogin.setOnClickListener{
             val intent = Intent(this, logindanregister::class.java)
@@ -28,6 +41,15 @@ class RegisterActivity2 : AppCompatActivity() {
         binding.btnRegister.setOnClickListener {
             val email = binding.edtEmailRegister.text.toString()
             val password = binding.edtPasswordRegister.text.toString()
+            val nama = binding.edtNamaRegister.text.toString()
+            val phone = binding.edtNomorhpRegister.text.toString()
+
+            val cekGenderRadioButtonId = gender1.checkedRadioButtonId
+            val listGender = findViewById<RadioButton>(cekGenderRadioButtonId)
+
+            val hasilGender = "${listGender.text}"
+            gender.text = hasilGender
+
 
             //Validasi Email
             if (email.isEmpty()) {
@@ -55,17 +77,29 @@ class RegisterActivity2 : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            RegisterFirebase(email,password)
+            RegisterFirebase(email,password, nama, phone, hasilGender)
         }
 
 
     }
 
-    private fun RegisterFirebase(email: String, password: String) {
+    private fun RegisterFirebase(email: String, password: String, nama: String, phone: String, hasilGender: String) {
         auth.createUserWithEmailAndPassword(email,password)
             .addOnCompleteListener(this){
                 if (it.isSuccessful){
                     Toast.makeText(this, "Register Berhasil", Toast.LENGTH_SHORT).show()
+                    val hashMap = hashMapOf<String, Any>(
+                        "email" to email,
+                        "name" to nama,
+                        "phone" to phone,
+                        "gender" to hasilGender,
+                    )
+                    firestore.collection("users")
+                        .add(hashMap)
+                        .addOnFailureListener { exception ->
+                            Log.w(TAG, "Error adding document $exception")
+                        }
+
                     val intent = Intent (this, logindanregister::class.java)
                     startActivity(intent)
                 }else{
